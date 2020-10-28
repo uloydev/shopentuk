b6f6ef3fc54dd737682bb91d8c398896731e89ff
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2020-10-28 19:41:43 +0700

    [UPDATE] disable /register page

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,11 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@landingPage')->name('landing-page');
-Route::get('store/produk', 'StoreController@product')->name('store.product');
+Route::get('store/product', 'StoreController@product')->name('store.product');
 Route::get('store/voucher', 'StoreController@voucher')->name('store.voucher');
 
 Auth::routes();
+Route::permanentRedirect('register', 'login');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
