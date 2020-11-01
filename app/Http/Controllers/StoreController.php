5eefb33cfb460885ca0ebef52df23bc9acd13ca0
uloydev <uloydev@gmail.com>
2020-11-01 07:33:25 +0700

    [FIX] product filter (don't change StoreController)

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -14,69 +14,17 @@ class StoreController extends Controller
     {
         $this->categories = ProductCategory::all();
     }
-
-    protected function productWhere($column, $conditional, $value)
-    {
-        return Product::where($column, $conditional, $value);
-    }
-
+    
     public function product(Request $request)
     {
-        return view('store.product', $this->getFilteredProducts($request, false));
+        return view('store.product.index', $this->getFilteredProducts($request, false));
     }
-
+    
     public function voucher(Request $request)
     {
         return view('store.voucher', $this->getFilteredProducts($request, true));
     }
-
-    private function getFilteredProducts(Request $request, bool $isDigitalProduct)
-    {
-        $products = Product::with('productCategory')->whereHas('productCategory', function ($query) use ($isDigitalProduct){
-            $query->where('is_digital_product', $isDigitalProduct);
-        });
-        $bestProducts = clone $products;
-        $bestProducts = $bestProducts->inRandomOrder()->limit(3)->get();
-        $httpQuery = [];
-
-        if ($request->has('catId') && !empty($request->catId)) {
-            $httpQuery['catId'] = $request->catId;
-            $products = $products->where('category_id', $request->catId);
-        }
-
-        if ($request->has('subCatId') && !empty($request->subCatId)) {
-            $httpQuery['subCatId'] = $request->subCatId;
-            $products = $products->where('sub_category_id', $request->subCatId);
-        }
-
-        if ($request->has('search')) {
-            $httpQuery['search'] = $request->search;
-            $products = $products->where(function ($query) use ($request){
-                $query->where('title', 'like', '%'.$request->search.'%')
-                ->orWhere('description', 'like', '%'.$request->search.'%');
-            });
-        }
-
-        if ($request->has('sort')) {
-            $httpQuery['sort'] = $request->sort;
-            if ($request->sort == "cheap") {
-                $products = $products->orderBy('price', 'asc');
-            } else if ($request->sort == "expensive") {
-                $products = $products->orderBy('price', 'desc');
-            }
-        }
-
-        $products = $products->paginate(12);
-        $products->appends($httpQuery);
-
-        return view('store.product.index', [
-            'products' => $products,
-            'bestProducts' => $bestProducts,
-            'categories' => $this->categories,
-            'httpQuery' => $httpQuery,
-        ]);
-    }
-
+    
     public function showProduct($slug)
     {
         $productTitle = strtolower(ucwords(str_replace('-', ' ', $slug)));
@@ -87,4 +35,57 @@ class StoreController extends Controller
             'products' => $products
         ]);
     }
+    
+    private function productWhere($column, $conditional, $value)
+    {
+        return Product::where($column, $conditional, $value);
+    }
+    
+    private function getFilteredProducts(Request $request, bool $isDigitalProduct)
+    {
+        $products = Product::with('productCategory')->whereHas('productCategory', function ($query) use ($isDigitalProduct){
+            $query->where('is_digital_product', $isDigitalProduct);
+        });
+        $bestProducts = clone $products;
+        $bestProducts = $bestProducts->inRandomOrder()->limit(3)->get();
+        $httpQuery = [];
+        
+        if ($request->has('catId') && !empty($request->catId)) {
+            $httpQuery['catId'] = $request->catId;
+            $products = $products->where('category_id', $request->catId);
+        }
+        
+        if ($request->has('subCatId') && !empty($request->subCatId)) {
+            $httpQuery['subCatId'] = $request->subCatId;
+            $products = $products->where('sub_category_id', $request->subCatId);
+        }
+        
+        if ($request->has('search')) {
+            $httpQuery['search'] = $request->search;
+            $products = $products->where(function ($query) use ($request){
+                $query->where('title', 'like', '%'.$request->search.'%')
+                ->orWhere('description', 'like', '%'.$request->search.'%');
+            });
+        }
+        
+        if ($request->has('sort')) {
+            $httpQuery['sort'] = $request->sort;
+            if ($request->sort == "cheap") {
+                $products = $products->orderBy('price', 'asc');
+            } else if ($request->sort == "expensive") {
+                $products = $products->orderBy('price', 'desc');
+            }
+        }
+        
+        $products = $products->paginate(12);
+        $products->appends($httpQuery);
+        
+        return [
+            'products' => $products,
+            'bestProducts' => $bestProducts,
+            'categories' => $this->categories,
+            'httpQuery' => $httpQuery,
+        ];
+    }
+    
 }
