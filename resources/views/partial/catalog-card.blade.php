042f3644f4d6c1da9510d73ad1436f69d2c2a0c4
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2020-11-14 19:34:44 +0700

    nothing

--- resources/views/partial/catalog-card.blade.php
+++ resources/views/partial/catalog-card.blade.php
@@ -13,7 +13,8 @@
             <x-box-promo 
             heading="{{ Str::words($category->title, 1) }}" 
             subheading="{{ Str::words($category->description, 5) }}"
-            subheadClass="text-base" primary-btn-text="Lihat Sekarang" primary-btn-link="{{ route('store.product.index', ['catId' => $category->id]) }}"
+            subheadClass="text-base" primary-btn-text="Lihat Sekarang" 
+            primary-btn-link="{{ route('store.product.index', ['catId' => $category->id]) }}"
             primary-btn-type="bg-blue-300 hover:bg-blue-100 text-gray-800 border border-gray-400 rounded shadow font-architects" 
             />
         @endif
--- resources/views/store/product/list.blade.php
+++ resources/views/store/product/list.blade.php
@@ -1,16 +1,16 @@
 <div class="p-5 -mx-5 overflow-hidden grid grid-cols-1 gap-x-5 gap-y-10 md:grid-cols-2 lg:gap-x-8 lg:gap-y-10 justify-items-center {{ $addClass  }}">
-    @foreach ($products as $product)
+    @forelse ($products as $product)
         @if ($product->discount)
-            <x-card-product
-                product-img="{{ 'example.jpg' }}" 
-                product-name="{{ $product->title }}"
-                product-category="{{ $product->productCategory->title }}" 
-                product-category-id="{{ $product->productCategory->id }}" 
-                product-original-price="{{ $product->price }}"
-                product-final-price="{{ $product->discount->discounted_price }}"
-                product-rating="3" 
-                is-horizontal="false"
-                product-is-obral="true" />
+        <x-card-product
+            product-img="{{ 'example.jpg' }}" 
+            product-name="{{ $product->title }}"
+            product-category="{{ $product->productCategory->title }}" 
+            product-category-id="{{ $product->productCategory->id }}" 
+            product-original-price="{{ $product->price }}"
+            product-final-price="{{ $product->discount->discounted_price }}"
+            product-rating="3" 
+            is-horizontal="false"
+            product-is-obral="true" />
         @else
             <x-card-product 
                 product-img="{{ 'example.jpg' }}" 
@@ -22,5 +22,15 @@
                 is-horizontal="false"
                 product-is-obral="false" />
         @endif
-    @endforeach
+    @empty
+        <figure class="col-span-full flex justify-center flex-col items-center">
+            <img src="{{ asset('img/static/empty.png') }}" alt="Empty catalog" class="h-64">
+            <figcaption>
+                <p class="text-2xl text-center">
+                    Oops, currently we don't have any product. <br> 
+                    Please check it later
+                </p>
+            </figcaption>
+        </figure>
+    @endforelse
 </div>
