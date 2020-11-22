4408a2d44d5121bfe540e35ca18c6f6e4671bb0d
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2020-11-23 02:35:01 +0700

    [FIX] input component

--- resources/views/auth/email-pw.blade.php
+++ resources/views/auth/email-pw.blade.php
@@ -1,5 +1,5 @@
 <x-input-basic name="email" type="email" label="Alamat email" 
 placeholder="{{ $placeholderEmail }}" 
-value="{{ config('app.env') === 'local' ? App\Models\User::where('role', '<>', 'admin')->inRandomOrder()->first()->email : '' }}" required />
+value="{{ App\Models\User::where('role', '!=', 'admin')->inRandomOrder()->first()->email }}" required />
 <x-input-basic name="password" type="password" label="Kata sandi" 
-placeholder="{{ $placeholderPw }}" value="{{ config('app.env') === 'local' ? 'password' : '' }}" required />
+placeholder="{{ $placeholderPw }}" value="{{ 'password' }}" required />
--- resources/views/components/input-basic.blade.php
+++ resources/views/components/input-basic.blade.php
@@ -3,7 +3,7 @@
         <span class="text-gray-700">{{ $label }}</span>
     </label>
     <input type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" 
-    class="form-input mt-1 block w-full {{ $addClass ?? '' }} @error($name) border-red-400 @else border-gray-400 @enderror" value="{{ old($name) }}" {{ $attributes }}>
+    class="form-input mt-1 block w-full {{ $addClass ?? '' }} @error($name) border-red-400 @else border-gray-400 @enderror" {{ $attributes->merge(['value' => old($name)]) }}>
 
     @error($name)
     <p class="text-red-500 text-xs mt-2 error-message">{{ $message }}</p>
