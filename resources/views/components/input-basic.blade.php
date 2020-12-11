60888d9d780e32b6b3f074ee82f0eb3f3b4d459d
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2020-12-12 02:20:40 +0700

    nothing

--- resources/views/components/input-basic.blade.php
+++ resources/views/components/input-basic.blade.php
@@ -1,9 +1,9 @@
 <div class="mb-6 {{ $boxWidth }}">
     <label for="{{ $name }}" class="block mb-2">
-    <span class="text-gray-700">{{ $label }}</span>
+        <span class="text-gray-700">{{ $label }}</span>
     </label>
-    <input type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" 
-    class="form-input mt-1 block w-full {{ $addClass ?? '' }} @error($name) border-red-400 @else border-gray-400 @enderror" {{ $attributes->merge(['value' => old($name)]) }}>
+    <input type="{{ $type }}" id="{{ $name }}" name="{{ $name }}"
+    class="form-input mt-1 block w-full {{ $addClass ?? '' }} @error($name) border-red-400 @else border-gray-400 @enderror" {{ $attributes }}>
 
     @error($name)
     <p class="text-red-500 text-xs mt-2 error-message">{{ $message }}</p>
