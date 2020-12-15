1d506fe474f5cddf415c5e5a7d87896a054d5074
uloydev <uloydev@gmail.com>
2020-12-15 21:24:11 +0700

    [ADD] point history seeder

--- database/seeders/PointHistorySeeder.php
+++ database/seeders/PointHistorySeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\PointHistory;
+use App\Models\User;
+use Faker\Factory;
 
 class PointHistorySeeder extends Seeder
 {
@@ -13,6 +16,22 @@ class PointHistorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Factory::create();
+
+        $users = User::where('role', 'customer')->get();
+        foreach ($users as $user) {
+            for ($i=0; $i < 4; $i++) { 
+                do {
+                    $point = $faker->randomElement([-20, -10, 10, 20, 50, 100]);
+                } while ($user->point + $point < 0);
+                PointHistory::create([
+                    'value' => $point,
+                    'description' => $point < 0 ? 'penggunaan point' : 'hadiah point',
+                    'user_id' => $user->id
+                ]);
+                $user->point += $point;
+                $user->save();
+            }
+        }
     }
 }
