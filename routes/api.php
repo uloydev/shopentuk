a1023b0c0d16393c5b322723deb932ecab572819
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2020-12-15 20:10:04 +0700

    ignore it

--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::name('api.')->group(function(){
-    Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function(){
+Route::name('api.')->group(function () {
+    Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
         Route::get('dashboard', 'DashboardController')->name('dashboard');
         Route::resources([
             'all-category' => 'AllCategoryController',
