6a86f474168eab36b26c8bfa3bbe816eab06c65a
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2020-12-16 22:36:42 +0700

    [UPDATE] helper js

--- resources/assets/js/helper-module.js
+++ resources/assets/js/helper-module.js
@@ -69,11 +69,45 @@ export function openCloseModal (modalSelector) {
         modalEl.classList.add(...classToCloseModal)
 
         if (modalEl.querySelectorAll('input, textarea')) {
-            modalEl.querySelectorAll('input, textarea').forEach(inputOrTextarea => {
+            modalEl.querySelectorAll('input:not([name="_method"]):not([name="_token"]), textarea').forEach(inputOrTextarea => {
                 inputOrTextarea.value = ''
             })
         }
     }
 }
+
+/*!
+ * Get all of an element's parent elements up the DOM tree
+ * (c) 2019 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {Node}   elem     The element
+ * @param  {String} selector Selector to match against [optional]
+ * @return {Array}           The parent elements
+ */
+export const getParents = function (elem, selector) {
+
+	// Setup parents array
+	var parents = [];
+
+	// Get matching parent elements
+	while (elem && elem !== document) {
+
+		// If using a selector, add matching parents to array
+		// Otherwise, add all parents
+		if (selector) {
+			if (elem.matches(selector)) {
+				parents.push(elem);
+			}
+		} else {
+			parents.push(elem);
+		}
+
+		// Jump to the next parent node
+		elem = elem.parentNode;
+
+	}
+
+	return parents;
+
+};
 
 export const pageUrl = window.location.pathname
--- resources/assets/js/helper-utilities.js
+++ resources/assets/js/helper-utilities.js
@@ -1,39 +1,5 @@
 import * as Helper from './helper-module.js'
 
-/*!
- * Get all of an element's parent elements up the DOM tree
- * (c) 2019 Chris Ferdinandi, MIT License, https://gomakethings.com
- * @param  {Node}   elem     The element
- * @param  {String} selector Selector to match against [optional]
- * @return {Array}           The parent elements
- */
-export const getParents = function (elem, selector) {
-
-	// Setup parents array
-	var parents = [];
-
-	// Get matching parent elements
-	while (elem && elem !== document) {
-
-		// If using a selector, add matching parents to array
-		// Otherwise, add all parents
-		if (selector) {
-			if (elem.matches(selector)) {
-				parents.push(elem);
-			}
-		} else {
-			parents.push(elem);
-		}
-
-		// Jump to the next parent node
-		elem = elem.parentNode;
-
-	}
-
-	return parents;
-
-};
-
 
 /*
  * .input-lowercase utitlies to make all input lowercase
