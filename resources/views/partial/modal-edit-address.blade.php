9f2175634ed6115374b7357d6f7cf25211dd6b9e
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2020-12-16 22:34:45 +0700

    [UPDATE] implement more btn component

--- resources/views/partial/modal-edit-address.blade.php
+++ resources/views/partial/modal-edit-address.blade.php
@@ -26,13 +26,9 @@ class="fixed z-10 inset-0 overflow-y-auto transition duration-200 invisible h-0 
                 </div>
             </div>
             <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <button type="submit" form="editAddressForm" 
-                class="w-full inline-flex justify-center rounded-md border border-transparent
-                shadow-sm px-4 py-2 bg-teal-400 text-base font-medium text-white
-                hover:bg-teal-500 focus:outline-none focus:ring-2 focus:ring-offset-2
-                focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm next-step">
-                    Submit
-                </button>
+                <x-btn action="submit" type="primary" form="editAddressForm" text="Submit"
+                add-class="w-full lg:w-auto next-step" />
+                
                 <button type="button" 
                 class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 
                 shadow-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none px-4 
