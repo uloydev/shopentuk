29273bd0b79763ade8ca208e3a6a84c4165b96ef
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2020-12-18 03:20:47 +0700

    [UPDATE] tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,39 @@
 module.exports = {
   // important: true,
-  future: {
-    removeDeprecatedGapUtilities: true,
-  },
-  purge: [],
-  theme: {
-    fontFamily: {
-      sans: [
-        'system-ui',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        '"Noto Sans"',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
-        'Nunito'
-      ],
-      serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
-      mono: ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
-      cursive: ['"Annie Use Your Telescope"'],
-      roboto: ['Roboto'],
+    future: {
+        removeDeprecatedGapUtilities: true,
     },
-    container: {
-      center: true,
-      padding: '1.25rem'
+    purge: [],
+    theme: {
+        container: {
+            center: true,
+            padding: '1.25rem'
+        },
+        fontFamily: {
+            sans: [
+                'system-ui',
+                '-apple-system',
+                'BlinkMacSystemFont',
+                '"Segoe UI"',
+                'Roboto',
+                '"Helvetica Neue"',
+                'Arial',
+                '"Noto Sans"',
+                'sans-serif',
+                '"Apple Color Emoji"',
+                '"Segoe UI Emoji"',
+                '"Segoe UI Symbol"',
+                '"Noto Color Emoji"',
+                'Nunito'
+            ],
+            serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+            mono: [
+                'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'
+            ],
+            cursive: ['"Annie Use Your Telescope"'],
+            roboto: ['Roboto'],
+        },
     },
-  },
   variants: {
     borderWidth: ['responsive', 'hover', 'focus'],
     backgroundColor: ['responsive', 'hover', 'focus', 'active'],
