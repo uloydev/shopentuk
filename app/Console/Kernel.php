02e838da12ab768af08f3a1d9af2910c591abd91
uloydev <uloydev@gmail.com>
2021-01-10 00:16:11 +0700

    [ADD] game scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,11 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
+
+use App\Models\Game;
+use App\Models\GameBid;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,7 +29,37 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            Log::info('test '.Carbon::now());
+            if (Game::all()->isEmpty()) {
+                $now = Carbon::now();
+                $now->second = 0;
+                while ($now->minute % 3 != 0) {
+                    $now->minute++;
+                }
+                // create new game
+                for ($i=0; $i < 5; $i++) { 
+                    $game = new Game();
+                    $game->started_at = $now;
+                    $now->addMinute(3);
+                    $game->ended_at = $now;
+                    $game->status = 'queued';
+                    $game->save();
+                }
+                Game::first()->update(['status' => 'playing']);
+            } else {
+                // update game status
+                Game::where('status', 'playing')->update(['status' => 'finished']);
+                Game::where('status', 'queued')->first()->update(['status' => 'playing']);
+                // create new game
+                $lastGame = Game::latest()->first();
+                Game::create([
+                    'started_at' => $lastGame->ended_at,
+                    'ended_at' => $lastGame->ended_at->addMinute(3),
+                ]);
+            }
+            Log::info('Game Count = '. Game::count());
+        })->cron('*/3 * * * *');
     }
 
     /**
