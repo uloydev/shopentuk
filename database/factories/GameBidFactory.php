0f86ecbd8276c83a664f3360d9ced696385e6bd8
uloydev <uloydev@gmail.com>
2021-03-04 11:59:54 +0700

    [UPDATE] Game Winner Logic

---
+++ database/factories/GameBidFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\GameBid;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class GameBidFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = GameBid::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
