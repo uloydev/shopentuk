537bb1d1f094547d214498221ef91f7e31c6d5fe
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-06-06 14:35:14 +0700

    [FIX] height game sidebar on mobile

--- resources/views/game/sidebar.blade.php
+++ resources/views/game/sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="bg-gray-300 w-full lg:w-3/12 lg:border-r-2 order-first mt-0 h-full">
+<aside class="bg-gray-300 w-full lg:w-3/12 lg:border-r-2 order-first mt-0 lg:h-full">
     <ul class="flex h-100 flex-col">
         <li class="text-lg">
             <a href="javascript:void(0)" class="flex items-center lg:w-auto px-4 w-1/3 py-4 capitalize cursor-pointer cursor-default w-full">
